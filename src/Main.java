1f4b55a3de0ae9c055b7cfe87627a7675b226b33
zarigata <Ferrary@12>
2023-11-14 04:24:44 -0300

    1GEN

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Tank myTank = new Tank();
+        myTank.Name = "Panzerkranfwagent AuFs1";
+        myTank.Year = 1941;
+        myTank.Rank = 6.3;
+
+        System.out.println(myTank.Name);
+        System.out.println(myTank.Year);
     }
 }
